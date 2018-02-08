2f59fe887085595c9fb1ccad92b67f7253250006
Ryan C <ryanjchen2@gmail.com>
2018-02-07 22:04:20 -0500

    did hella python

---
+++ import.py
@@ -0,0 +1,34 @@
+from  pymongo import MongoClient
+from openpyxl import Workbook
+from openpyxl import load_workbook
+client = MongoClient()
+
+client = MongoClient("mongodb://localhost:27017")
+
+db = client["test"]
+money = db.money
+wb = load_workbook(filename='./2016_test.xlsx')
+print(wb.sheetnames)
+
+sheet = wb.active
+
+#print(sheet['A1'].value)
+
+data_list = []
+
+for row in sheet.iter_rows(min_row=2, min_col=1, max_row=1000, max_col=14):
+    a = []
+    for cell in row:
+        a.append(cell.value)
+    post_data = {
+            'last_name': a[2],
+            'first_name': a[3],
+            'middle_name': a[4],
+            'dept': a[5],
+            'employee_group': a[10],
+            'compensation': a[11]
+    }
+    data_list.append(post_data)
+
+#insert the entire fucking list yooooo
+db.money.insert_many(data_list)
