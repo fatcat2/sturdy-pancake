f06908608acefbf7e87650242598d3364e4914b1
Ryan Chen <ryanjchen@yahoo.com>
2018-03-04 01:25:12 -0500

    Added some quicksort

--- main.js
+++ main.js
@@ -10,6 +10,36 @@ const dbname = 'test';
 // var list = [];
 var listchunks = [];
 var chunk1;
+
+function swap(arr, index_1, index_2){
+	var temp = arr[index_1];
+	arr[index_1] = arr[index_2];
+	arr[index_2] = temp;
+}
+
+function partition(arr, low, high){
+	var pivot = arr[4][high];
+	i = low-1;
+
+	for (var j = low; j >= high; j++) {
+		if (arr[j]) {
+			i++;
+			swap(arr, i, j);
+		}
+	}
+	swap(arr, i+1, high);
+	return (i+1);
+}
+
+function quickSort(arr, low, high){
+	if(low < high){
+		var partition = parition(arr, low, high)
+		quickSort(arr, low, partition-1);
+		quickSort(arr, partition+1, high);
+	}
+}
+
+
 app.set('view engine', 'pug');
 
 app.get("/", function (req, res){
@@ -35,6 +65,30 @@ app.get("/", function (req, res){
 	// res.render('main', {description: 'saucy', list: list} )
 });
 
-app.get("/sort", function (req, res))
+app.get("/sort/salary", function (req, res){
+	//get the list
+	MongoClient.connect(url, function(err, client){
+		assert.equal(null, err);
+		console.log("Connected successfully to the server");
+		
+		const db = client.db(dbname);
+		db.collection('money').find({}).toArray(function(err, result){
+			var list = [];
+			// console.log(err);
+			if(err) throw err;
+			for(var i = 0; i < 50; i++){
+				row = result[i];
+				list.push([row.last_name, row.first_name, row.dept, row.employee_group, row.compensation]);
+			}
+
+			// list = [1, 2, 3];
+			res.render('main', {description: 'saucy', list: list} );
+			client.close();
+		});
+	});
+	//sort the list
+	//render the motherfucking list
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
