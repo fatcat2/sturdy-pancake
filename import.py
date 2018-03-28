ed68fe9bca76c0ce08df724608f093c18d6c295e
Ryan Chen <ryanjchen@yahoo.com>
2018-03-28 13:12:16 -0400

    new design

--- import.py
+++ import.py
@@ -6,8 +6,8 @@ import os
 client = MongoClient('mongodb+srv://exponent_cand:'+os.environ.get('password')+'@cluster0-ufki0.mongodb.net/test')
 
 db = client["test"]
-money = db.seventeen
-wb = load_workbook(filename='2017.xlsx')
+money = db.sixteen
+wb = load_workbook(filename='2016.xlsx')
 print(wb.sheetnames)
 
 sheet = wb.active
@@ -16,19 +16,19 @@ print(sheet['B2'].value)
 
 data_list = []
 
-for row in sheet.iter_rows(min_row=2, min_col=1, max_row=16695, max_col=11):
+for row in sheet.iter_rows(min_row=2, min_col=3, max_row=12947, max_col=12):
     a = []
     for cell in row:
         a.append(cell.value)
     print type(a)
     post_data = {
-            'last_name': a[1],
-            'first_name': a[2],
-            'middle_name': a[4],
-            'dept': a[4],
-            'city': a[6],
-            'employee_group': a[9],
-            'compensation': a[10]
+            'last_name': a[0],
+            'first_name': a[1],
+            'middle_name': a[2],
+            'dept': a[3],
+            'city': a[5],
+            'employee_group': a[8],
+            'compensation': a[9]
             }
     data_list.append(post_data)
     #insert the entire fucking list yooooo
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+# imports are listed below
 from flask import Flask
 import openpyxl as excel
 from pymongo import MongoClient
 from flask import render_template
 import os
+
+# declare the flask app object
 app = Flask(__name__)
+
+# route makes it so when you go to that specific url it will render the index template
 @app.route('/')
 def hello():
     url = 'mongodb+srv://exponent_cand:'+os.environ.get('password')+'@cluster0-ufki0.mongodb.net/test'
@@ -13,4 +18,15 @@ def hello():
     money_list = list(collection.find())
     client.close()
     print money_list[0]
-    return render_template('index.html', moneyList=money_list)
+    return render_template('index.html', downyear=2016, year=2017, moneyList=money_list)
+
+@app.route('/2016')
+def sixteen():
+    url = 'mongodb+srv://exponent_cand:'+os.environ.get('password')+'@cluster0-ufki0.mongodb.net/test'
+    client = MongoClient(url)
+    db = client.test
+    collection = db.sixteen
+    money_list = list(collection.find())
+    client.close()
+    print money_list[0]
+    return render_template('index.html', downyear=2015, year=2016, moneyList=money_list)
