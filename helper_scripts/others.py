73d4cf266c166d294dcfbd8dab7060ac23383fac
Ryan Chen <ryanjchen@yahoo.com>
2018-07-03 00:25:46 -0700

    added historical salary data to db file years 2011-2015

--- helper_scripts/others.py
+++ helper_scripts/others.py
@@ -1,7 +1,7 @@
 import json
 import sqlite3
 from openpyxl import Workbook, load_workbook
-wb = load_workbook(filename='2011.xlsx')
+wb = load_workbook(filename='../excel/2015.xlsx')
 
 sheet = wb.active
 
@@ -11,18 +11,16 @@ data_list = []
 conn = sqlite3.connect("salary_history.db")
 c = conn.cursor()
 
-x = 0
+x = 58000
 
-
-
+print "adding data"
 #go through excel file and add entries to sqlite3 db
-for row in sheet.iter_rows(min_row=2, min_col=1, max_row=16621, max_col=13):
+for row in sheet.iter_rows(min_row=2, min_col=1, max_row=12947, max_col=13):
     a = []
     for cell in row:
         a.append(cell.value)
     compensation_json = {}
-    compensation_json["2011"] = a[12]
-    # print json.dumps(compensation_json)
+    compensation_json["2015"] = a[11]
 
     if a[4] == None:
         a[4] = ""
@@ -36,6 +34,7 @@ for row in sheet.iter_rows(min_row=2, min_col=1, max_row=16621, max_col=13):
                 "middle_name": a[4],
                 "dept": a[5],
                 "employee_group": a[10],
+                "compensation_num": a[11],
                 "compensation": json.dumps(compensation_json)
                 }
     except Exception as e:
@@ -49,10 +48,25 @@ for i in range(0, len(data_list)):
     person = data_list[i]
     try:
         insert_data = (person["id"], person["name"], person["last_name"], person["first_name"], person["middle_name"], person["dept"], person["employee_group"], person["compensation"])
-        c.execute("insert into salary values(?, ?, ?, ?, ?, ?, ?, ?)", insert_data)
+        c.execute("select compensation from salary where name=?", (person["name"],))
+        data = c.fetchone()
+        # print data[0]
+        if data:
+            print person["name"] + " found in database"
+            # print data[0]
+            json_data = json.loads(data[0])
+            # print type(json_data)
+            json_data["2015"] = person["compensation_num"]
+            print json_data["2015"], json_data
+            c.execute("update salary set compensation=? where name=?", (str(json.dumps(json_data, sort_keys=True)), person["name"]))
+        else:
+            print "----------------"
+            print person["name"] + " not found in db"
+            print "----------------"
+            c.execute("insert into salary values (?, ?, ?, ?, ?, ?, ?, ?)", (insert_data))
     except Exception as e:
         # print person
         print e
-    conn.commit()
 
+conn.commit()
 conn.close()
--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 # imports are listed below
 from flask import Flask, request, url_for
-import openpyxl as excel
 from pymongo import MongoClient
 from flask import render_template
+import openpyxl as excel
 import os
+import sqlite3
+import json
 
 # declare the flask app object
 app = Flask(__name__)
 
 # test route for the new ajax option
-@app.route('/dev')
+@app.route("/dev")
 def dev():
-	print 'dev page has been accsessed'
-	return render_template('dev.html')
+	print "dev page has been accsessed"
+	return render_template("dev.html")
 
-@app.route('/data')
+@app.route("/data")
 def data():
     x = request.values
     for key,value in x.iteritems():
@@ -23,16 +25,26 @@ def data():
 	
 
 # route makes it so when you go to that specific url it will render the index template
-@app.route('/')
+@app.route("/")
 def hello():
-    return render_template('index.html', year="2017" )
+    return render_template("index.html", year="2017" )
 
-@app.route('/<page>')
+# renders the page for the specific year
+@app.route("/<page>")
 def not_current_year(page):
-    return render_template('index.html', year=page)
+    return render_template("index.html", year=page)
 
-@app.route('/sports')
+# this one is for the sports desk #alleyes
+@app.route("/sports")
 def sports():
-    return render_template('sports.html')
+    return render_template("sports.html")
 
-
+#renders page for individual salary
+@app.route("/salary/<name>", methods=["POST"])
+def individualSalary(name):
+    conn = sqlite3.connect("salary_history.db")
+    c = conn.cursor()
+    c.execute("select * from salary where name=?", (name))
+    data = c.fetchone()
+    salary_data = json.loads(data["compensation"])
+    return render_template("salary.html", data=data, salary=salary_data)
