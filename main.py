e17951bcdb958623914c1c7d70379b866cfa4583
Ryan Chen <ryanjchen@yahoo.com>
2018-08-20 18:16:46 -0400

    Made some changes to follow exponent guidelines

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ app = Flask(__name__)
 def dev():
 	return(render_template("dev.html"))
 
+@app.route("/about")
+def about():
+	return(render_template("about.html"))
+
 @app.route("/data")
 def data():
     x = request.values
