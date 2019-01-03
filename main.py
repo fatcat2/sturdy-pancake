9645b33acfd9eb943d34c01837ed80e54e8e1d5b
Ryan Chen <ryanjchen2@gmail.com>
2019-01-03 14:18:53 -0800

    Added favicon and is functional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # imports are listed below
 from flask import Flask, request, url_for
-from flask import render_template
+from flask import render_template, send_from_directory
 import os
 import sqlite3
 import json
@@ -9,9 +9,10 @@ import json
 app = Flask(__name__)
 
 # test route for the new ajax option
-@app.route("/dev")
-def dev():
-	return(render_template("dev.html"))
+@app.route("/favicon.ico")
+def favicon():
+    print("Favicon requested")
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route("/about")
 def about():
