4f59a929623ea624cf470a21c7738fffc733557f
Ryan Chen <ryanjchen2@gmail.com>
2020-10-04 18:44:29 -0400

    Removed unused menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,19 +28,6 @@ const { SubMenu } = Menu;
 const { Search } = Input;
 const { Title, Paragraph, Text } = Typography;
 
-const menu = (
-    <Menu>
-        <Menu.Item key="0">
-            <a href="http://www.alipay.com/">1st menu item</a>
-        </Menu.Item>
-        <Menu.Item key="1">
-            <a href="http://www.taobao.com/">2nd menu item</a>
-        </Menu.Item>
-        <Menu.Divider />
-        <Menu.Item key="3">3rd menu item</Menu.Item>
-    </Menu>
-);
-
 const yearOptions = [
     {
         key: '2019',
